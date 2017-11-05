4450246253acfbbecd1221e220e1491715074d22
Apisit Asavatappitak <apisit@Apisits-MacBook-Pro.local>
2017-11-05 21:35:44 +0700

    add service worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,18 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js').then(() => {
-		console.log('Service worker registered!')
-	})
+let deferredPrompt
+
+if (!window.Promise) {
+	window.Promise = Promise
 }
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js')
+		.then(() => console.log('Service worker registered!'))
+		.catch(err => console.log(err))
+}
+
+window.addEventListener('beforeinstallprompt', event => {
+	console.log('beforeinstallprompt fired')
+	event.preventDefault()
+	deferredPrompt = event
+	return false
+})
--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,45 @@
 self.addEventListener('install', event => {
 	console.log('[Service Worker] install', event)
+
+	event.waitUntil(
+		caches.open('static').then(cache => {
+			cache.addAll([
+				'/',
+				'/index.html',
+				'/src/js/app.js',
+				'/src/js/feed.js',
+				'/src/js/promise.js',
+				'/src/js/fetch.js',
+				'/src/js/material.min.js',
+				'/src/css/app.css',
+				'/src/css/feed.css',
+				'/src/images/main-image.jpg',
+				'https://fonts.googleapis.com/css?family=Roboto:400,700',
+				'https://fonts.googleapis.com/icon?family=Material+Icons',
+				'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+			])
+		})
+	)
 })
 
 self.addEventListener('activate', event => {
-	console.log('[Service Worker] activate', event)
-
 	return self.clients.claim()
 })
 
 self.addEventListener('fetch', event => {
-	console.log('[Service Worker] fetching', event)
-	event.respondWith(fetch(event.request))
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			if (response) {
+				return response
+			} else {
+				return fetch(event.request).then(res => {
+					return caches.open('dynamic').then(cache => {
+						cache.put(event.request.url, res.clone())
+
+						return res
+					})
+				})
+			}
+		})
+	)
 })
