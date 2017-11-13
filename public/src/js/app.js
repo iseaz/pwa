5d43126c649f6838efd968f118f010c40b6d90ae
Apisit Asavatappitak <apisit@ADAPTERDIGITAL.LOCAL>
2017-11-13 14:29:11 +0700

    clean up cache

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,7 +5,8 @@ if (!window.Promise) {
 }
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js')
+	navigator.serviceWorker
+		.register('/sw.js')
 		.then(() => console.log('Service worker registered!'))
 		.catch(err => console.log(err))
 }
--- public/sw.js
+++ public/sw.js
@@ -2,27 +2,40 @@ self.addEventListener('install', event => {
 	console.log('[Service Worker] install', event)
 
 	event.waitUntil(
-		caches.open('static').then(cache => {
-			cache.addAll([
-				'/',
-				'/index.html',
-				'/src/js/app.js',
-				'/src/js/feed.js',
-				'/src/js/promise.js',
-				'/src/js/fetch.js',
-				'/src/js/material.min.js',
-				'/src/css/app.css',
-				'/src/css/feed.css',
-				'/src/images/main-image.jpg',
-				'https://fonts.googleapis.com/css?family=Roboto:400,700',
-				'https://fonts.googleapis.com/icon?family=Material+Icons',
-				'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-			])
-		})
+		caches.open('static-v2')
+			.then(cache => {
+				cache.addAll([
+					'/',
+					'/index.html',
+					'/src/js/app.js',
+					'/src/js/feed.js',
+					'/src/js/promise.js',
+					'/src/js/fetch.js',
+					'/src/js/material.min.js',
+					'/src/css/app.css',
+					'/src/css/feed.css',
+					'/src/images/main-image.jpg',
+					'https://fonts.googleapis.com/css?family=Roboto:400,700',
+					'https://fonts.googleapis.com/icon?family=Material+Icons',
+					'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+				])
+			})
 	)
 })
 
 self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches.keys()
+			.then(keyList => {
+				return Promise.all(keyList.map(key => {
+					if (key !== 'static-v2' && key !== 'dynamic') {
+						console.log('[Service worker] Removing old cache', key)
+						return caches.delete(key)
+					}
+				}))
+			})
+	)
+
 	return self.clients.claim()
 })
 
@@ -38,6 +51,8 @@ self.addEventListener('fetch', event => {
 
 						return res
 					})
+				}).catch(err => {
+					console.log(err, 'rrr')
 				})
 			}
 		})
